e20f96fec12274a304d1f004f649214ec595660b
Lmilne <lmilne360@gmail.com>
2017-06-18 01:12:18 -0400

    Added  different skip message once all slides are viewed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 import { MainPage } from '../main/main';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-	skipMsg: string = "Skip"
+	skipMsg: string = "Skip";
+	@ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController) {
 
@@ -15,6 +17,12 @@ export class HomePage {
 
   skip(){
   	this.navCtrl.push(MainPage)
+  }
+
+  slideChanged() {
+  	if (this.slides.isEnd){
+  		this.skipMsg= "Alright, I got it";
+  	}
   }
 
 }
